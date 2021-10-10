2c3b8ba4188938962a5e963e1401a18e49252722
Jacob M <58337545+Jacob-Python@users.noreply.github.com>
2021-10-10 18:17:22 -0400

    fix css

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 5000))
 c = CORS(app)
-questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Glorious Leader's concrete slab", "Based independent territory", 0, "What is this? <img src=\"/static/taiwan.png\">"], ["Indoctrination", "Playtime with Great Leader", 1, "What is happening here? <img src=\"uighurs.png\">"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
+questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Glorious Leader's concrete slab", "Based independent territory", 0, "What is this? <img class=\"img\" src=\"/static/taiwan.png\">"], ["Indoctrination", "Playtime with Great Leader", 1, "What is happening here? <img class=\"img\" src=\"uighurs.png\">"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
 results = [15, 150, 1500, 15000, 150000]
 
 @app.route("/")
