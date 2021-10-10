f510dfc288551904248bcd491246f6a1b3ca9b5b
Jacob M <58337545+Jacob-Python@users.noreply.github.com>
2021-10-10 18:47:26 -0400

    try to fix img bug

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 5000))
 c = CORS(app)
-questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Glorious Leader's concrete slab", "Based independent territory", 0, "What is this? <img class=\"img\" src=\"/static/taiwan.png\">"], ["Indoctrination", "Playtime with Great Leader", 1, "What is happening here? <img class=\"img\" src=\"uighurs.png\">"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
+questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Glorious Leader's concrete slab", "Based independent territory", 0, "What is this? #taiwan.png#"], ["Indoctrination", "Playtime with Great Leader", 1, "What is happening here? #uighurs.png#"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
 results = [15, 150, 1500, 15000, 150000]
 
 @app.route("/")
@@ -16,7 +16,7 @@ def home():
 @app.route("/question")
 def question():
     i=int(request.args["index"])
-    return Markup(json.dumps([results[i], questions[i]]))
+    return json.dumps([results[i], questions[i]])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
