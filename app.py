3f418696acd266d178ba60d8309d64a6f58310d4
Jacob M <58337545+Jacob-Python@users.noreply.github.com>
2021-10-10 23:08:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 5000))
 c = CORS(app)
-questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Supreme Leader's clay", "Based independent territory", 0, "What is this? #taiwan-china.png#"], ["Indoctrination", "Playtime with Supreme Leader", 1, "What is happening here? #uighurs.png#"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
+questions = [["nothing", "[REDACTED]", 0, "What happened in tiananmen square in 1989?"], ["Joe Biden", "Xi Jinping", 1, "Who is the best?"], ["Supreme Leader's clay", "Based independent territory", 0, "What is this? #taiwan-china.png#"], ["Re-education camp", "Supreme Leader's playground", 1, "What is happening here? #uighurs.png#"], ["None, because I work at factory all day", "All of them", 0, "How many hours did you spent gaming?"]]
 results = [15, 150, 1500, 15000, 150000]
 
 @app.route("/")
